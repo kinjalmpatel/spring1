acca85cb8b286685fcf727bfc61a1874936c2240
kinjalmpatel <kinjalmpatel1408@gmail.com>
2021-10-08 14:41:06 +0530

    first

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,7 +74,19 @@ public class EmployeeController {
 		   experienceService.saveExperience(experience);
 		}
 		Employee updatedEmplopyee=employeeRepository.save(employee);
-	return ResponseEntity.ok(updatedEmplopyee);
-		
+	return ResponseEntity.ok(updatedEmplopyee);		
 	}
+	@DeleteMapping("employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id)
+	{
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow();
+		employeeRepository.delete(employee);
+		Map<String, Boolean> responce = new HashMap<>();
+		responce.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(responce);
+	}
+	
+	
+	
 }
